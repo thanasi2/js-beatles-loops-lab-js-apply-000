83d330d81c68d4a1b040e026c4552a72edb3e3f6
Thanasi Grizis <thanasi2@gmail.com>
2020-04-07 15:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,9 +13,11 @@ function theBeatlesPlay() {
 
 function johnLennonFacts() {
   const facts = [
-  "While on tour, he enjoyed playing Monopoly",
-  "Harmonica was the first instrument Lennon learned to play"
-  ];
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
   var factsExclaim = [];
   var x = 0;
   while (x < facts.length) {
